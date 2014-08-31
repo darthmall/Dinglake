2ae07f2246de65c4ef247ea30d61a5208480b70d
W. Evan Sheehan <wallace.sheehan@gmail.com>
2014-08-30 21:46:33 -0400

    Style the UI.
    Add a custom Vue filter to interpret the keycodes into arrow keys. Add some
    borders to the sound lists. Remove the hero-unit CSS.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,18 @@
 /* global Dinglake, Vue */
 (function () {
     'use strict';
-    
+
+    Vue.filter('keycode', function (value) {
+        var code = value.charCodeAt(0),
+            arrows = ['←', '↑', '→', '↓'];
+
+        if (code < 37 || code > 40) {
+            return value.toUpperCase();
+        }
+
+        return arrows[code - 37];
+    });
+
     $.ajax({
         url: 'config.json',
         dataType: 'json',
